77a5ed20c86d87723fbe6ad561860927d2bcd1d3
Hebadababa <fcorn795@gmail.com>
2023-11-02 20:06:10 +0400

    chap 4 exercise 2,3

---
+++ Chapter 4- Control Flow/Exercises/exercise 2.py
@@ -0,0 +1,15 @@
+#if block
+alien_color='green'
+if alien_color=='green':
+    print('Congratulations!,You just earned 5 points for shooting the alien')
+else:
+    print('Congratulations!,You just earned 10 points for shooting te alien')
+
+
+ #else block
+alien_color ='red'
+if alien_color=='green':
+    print('congratulations!,you just earned 5 points for shooting the alien')
+else:
+    print('congratulations!,you just earned 10 points for shooting the alien')
+    
---
+++ Chapter 4- Control Flow/Exercises/exercise 3.py
@@ -0,0 +1,8 @@
+alien_color='red'
+if alien_color=='green':
+     print('Congratulations!,you just earned 5 points')
+elif alien_color=='yellow':
+     print('Congratulations!,you just earned 10 points')
+else:
+     print('Congratulations!,you just earned 15 points')
+     
